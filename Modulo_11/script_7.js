cbdb637c9dc6023e3755ddd708de1ecaf18aa5f4
Ivan Oliveira <79813537+IvanIndaia@users.noreply.github.com>
2022-01-06 14:22:04 -0300

    Up.06.01-morning

--- Modulo_11/script_7.js
+++ Modulo_11/script_7.js
@@ -1,14 +1,12 @@
 let tela = document.getElementById("tela")
 let ctx = tela.getContext("2d")
 
+
 ctx.moveTo(0,0)
 ctx.lineTo(250, 250)
 ctx.lineTo(500, 0)
 ctx.lineWidth = 5
 ctx.strokeStyle = "gray"
 ctx.stroke()
-
-
-
 
 
