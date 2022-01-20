8cd5fb4c5f79a8bb4cc2e9a6975f2ed61aae89ab
Ivan Oliveira <79813537+IvanIndaia@users.noreply.github.com>
2022-01-20 14:30:03 -0300

    Up.20.01

--- Modulo_14/script_01.js
+++ Modulo_14/script_01.js
@@ -1,10 +1,50 @@
-"use strict"
-// x = 8
+//STRICT MODE
+//--------------
+// "use strict"
+// // x = 8
 
-function imprimir() {
-    x = 7
-    console.log(x)
+// function imprimir() {
+//     x = 7
+//     console.log(x)
+// }
+
+// imprimir()
+// console.log(x)
+
+
+//THIS E BIND
+//--------------
+// function speakGeneric() {
+//     console.log(this.sound)
+// }
+
+// let dog = {
+//     sound: "Au Auuuu",
+//     speak: speakGeneric
+// }
+
+// let cat = {
+//     sound: "Miauuuu",
+//     speak: speakGeneric
+// }
+
+// let binderFunction = speakGeneric.bind(dog)
+
+// dog.speak()
+// cat.speak()
+
+//ARROW FUNCTIONS
+//--------------
+
+let dobro = x => 2 * this.x
+
+// let dobro = function() {
+//     return 2 * this.x
+// }
+
+let numero = {
+    x: 17,
+    d: dobro
 }
 
-imprimir()
-console.log(x)
+console.log(numero.d())
