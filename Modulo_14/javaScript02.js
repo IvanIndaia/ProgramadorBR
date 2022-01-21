cfd7e8897587c7451d9786e9c90b58c2441dd82f
Ivan Oliveira <79813537+IvanIndaia@users.noreply.github.com>
2022-01-21 14:12:03 -0300

    Up.21.01

---
+++ Modulo_14/javaScript02.js
@@ -0,0 +1,30 @@
+let usuarios = ["Adriano", "Marcia", "José"]
+
+        function inserirUser(nome) {
+            usuarios.push(nome)
+        }
+
+        function listarUser(){
+            console.log(usuarios)
+        }
+
+        inserirUser("Ivan")
+        listarUser()
+        
+
+         // let dobro = function (n) {return 2* n}
+
+        // function ola() {
+        //     console.log("Olá")
+        // }
+
+        // function Xau() {
+        //     console.log("Tchau")
+        // }
+
+        // function saudacao(s, nome) {
+        //     s()
+        //     console.log(nome)
+        // }
+
+        // saudacao(Xau, "Igor")
