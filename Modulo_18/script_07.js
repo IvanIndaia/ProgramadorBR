c37f6e7915706a3d2af4c2e71f10b49bcacc9f77
Ivan Oliveira <79813537+IvanIndaia@users.noreply.github.com>
2022-04-25 21:31:01 -0300

    Up-25.04

--- Modulo_18/script_07.js
+++ Modulo_18/script_07.js
@@ -7,3 +7,14 @@ $("h1").css({
     "background-color": "red",
     "font-size": "70px"
 })
+
+let box = $(".box")
+
+box.width(600)
+
+box.append(
+    "Largura:  ", box.width(), "<br>",
+    "Largura + padding: ", box.innerWidth(), "<br>",
+    "Largura + padding + borda: ", box.outerWidth(), "<br>",
+    "Largura + padding + borda + margem: ", box.outerWidth(true)
+)
