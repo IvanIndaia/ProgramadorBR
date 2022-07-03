41c2b4ba6c67d30c7db022099f2df2125304605e
Ivan Oliveira <79813537+IvanIndaia@users.noreply.github.com>
2022-07-02 22:02:15 -0300

    Hup

--- Modulo_22_Node-Express/installing-Node/index.js
+++ Modulo_22_Node-Express/installing-Node/index.js
@@ -3,7 +3,25 @@ const path = require('path')
 
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'client')))
+app.use("/meusite", express.static(path.join(__dirname, 'client')))
+app.get("/", (req, res) => {
+
+    // res.set("Content-Type", "text/html")
+    res.type('html')
+    res.send("<h1>Hello World from GET</h1>")
+})
+
+app.post("/", (req, res) => {   
+    res.send("<h1>Hello World from POST</h1>")
+})
+
+app.put("/", (req, res) => {   
+    res.send("<h1>Hello World from PUT</h1>")
+})
+
+app.delete("/", (req, res) => {   
+    res.send("<h1>Hello World from DELETE</h1>")
+})
 
 const PORT = 5000
 app.listen(PORT, () => {
